807ee5eb146d3adc857e878367c467f6bcd941f1
Ida Tallgren <ida.tallgren@hotmail.com>
2023-05-31 16:11:31 +0300

    Add sorting functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import "./App.css";
 import { useState } from "react";
 import beers from "./beers";
 import beerCategories from "./beerCategories";
 
-import Header from "./components/header";
-import Card from "./components/card";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [beersToShow, setBeersToShow] = useState(beers);
-  const [filterActive, setFilterActive] = useState(true);
+  const [filterActive, setFilterActive] = useState(false);
+
+  var i = 0;
 
   const filterCategories = (category) => {
     const results = beers.filter((beer) => beer.category === category);
     setBeersToShow(results);
-    setFilterActive(false);
+    setFilterActive(true);
+  };
+
+  const sortBeers = (sortingMethod) => {
+    if (sortingMethod ===  "highToLow") {
+      const sortedBeers = beersToShow.slice(0).sort((a, b) => b.rating - a.rating);
+      setBeersToShow(sortedBeers);
+    } else if (sortingMethod === "lowToHigh") {
+      const sortedBeers = beersToShow.slice(0).sort((a, b) => a.rating - b.rating);
+      setBeersToShow(sortedBeers);
+    } else {
+      return;
+    }
   };
 
   return (
@@ -25,23 +38,38 @@ const App = () => {
           <h4>filter by category</h4>
           <button
             className="remove"
-            hidden={filterActive}
+            hidden={!filterActive}
             onClick={() => {
               setBeersToShow(beers);
-              setFilterActive(true);
+              setFilterActive(false);
             }}
           >
             <i className="fas fa-times"></i>
           </button>
           {beerCategories.map((category) => (
-            <button
-              href="#"
+            <button key={i++}
               className="category filter-category"
               onClick={() => filterCategories(category)}
             >
               {category}
             </button>
           ))}
+          <div className="sort">
+            <h4>sort by rating</h4>
+            <select
+              name="sorting"
+              id="sorting"
+              onChange={(event) => sortBeers(event.target.value)}
+            >
+              <option className="sortingOption" value="noSort"></option>
+              <option className="sortingOption" value="lowToHigh">
+                Lowest to highest
+              </option>
+              <option className="sortingOption" value="highToLow">
+                Highest to lowest
+              </option>
+            </select>
+          </div>
         </div>
         <div className="grid">
           {beersToShow.map((beer) => (
@@ -50,9 +78,12 @@ const App = () => {
         </div>
       </main>
       <Footer />
-      <a href="#" className="to-top">
+      <span
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        className="to-top"
+      >
         <i className="fas fa-arrow-up" />
-      </a>
+      </span>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,12 +8,5 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 reportWebVitals(sendToVercelAnalytics);
